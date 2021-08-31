d3329d871d0b6bfa0c57d7f7b1b6a7a25d67b7aa
Hiteke <38786753+Hiteke@users.noreply.github.com>
2021-09-01 00:49:37 +0900

    Add files via upload

---
+++ Bloody7Crack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Bloody7Crack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var application in Process.GetProcessesByName("Bloody7"))
+            {
+                application.Kill();
+                application.WaitForExit();
+            }
+
+            if (!File.Exists("Bloody7.exe"))
+            {
+                MessageBox.Show("File Bloody7.exe not found");
+                return;
+            }
+
+            if (File.Exists("Cracked.exe"))
+            {
+                File.Delete("Cracked.exe");
+            }
+
+            File.Copy("Bloody7.exe", "Original.exe");
+
+            var stream = File.Open("Bloody7.exe", FileMode.Open);
+            stream.Position = 2578955;
+            stream.WriteByte(0x85);
+            stream.Flush();
+            stream.Close();
+
+            Process.Start("Bloody7.exe");
+
+            File.Move("Bloody7.exe", "Cracked.exe");
+            File.Move("Original.exe", "Bloody7.exe");
+        }
+    }
+}
---
+++ Bloody7Crack/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bloody7Crack.exe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bloody7Crack.exe")]
+[assembly: AssemblyCopyright("Hiteke")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c995350d-9dae-48dc-9e1e-6aa6cce982e5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
